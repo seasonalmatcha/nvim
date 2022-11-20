950fa34e3fc0e60ab59f4f7f11607a13c29c572f
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-11-20 11:04:04 +0700

    fix: clipboard not working for wsl2

--- init.lua
+++ init.lua
@@ -1,3 +1,20 @@
 require 'options'
 require 'keymaps'
 require 'plugins'
+
+if vim.fn.has("wsl") then
+  vim.cmd [[
+   let g:clipboard = {
+      \   'name': 'wslclipboard',
+      \   'copy': {
+      \      '+': 'win32yank.exe -i --crlf',
+      \      '*': 'win32yank.exe -i --crlf',
+      \    },
+      \   'paste': {
+      \      '+': 'win32yank.exe -o --lf',
+      \      '*': 'win32yank.exe -o --lf',
+      \   },
+      \   'cache_enabled': 1,
+      \ }
+  ]]
+end
