a21b288cd93d17e91640fa622c060a97e6fe38ee
Alan H <me@alanh.dev>
2024-02-24 03:54:24 +0700

    feat: add auto-session

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,9 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      pre_save_cmds = { "Neotree close" },
+      post_restore_cmds = { "Neotree show" },
+    })
+  end,
+}
